7387a9c2ff1c6cf8856502b0a02aa4d78a46a35f
ljy <2736289464@qq.com>
2024-09-18 15:42:49 +0800

    finish match-client

--- game/src/client.py
+++ game/src/client.py
@@ -1,3 +1,4 @@
+
 from match_client.match import Match
 from match_client.match.ttypes import User
 
@@ -6,10 +7,12 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+from sys import stdin
 
-def main():
+
+def operate(op, user_id, username, score):
     # Make socket
-    transport = TSocket.TSocket('localhost', 9090)
+    transport = TSocket.TSocket('127.0.0.1', 9090)
 
     # Buffering is critical. Raw sockets are very slow
     transport = TTransport.TBufferedTransport(transport)
@@ -22,10 +25,24 @@ def main():
 
     # Connect!
     transport.open()
-    user = User(1, 'ljy', 1500)
-    client.add_user(user, "")
+
+    user = User(user_id, username, score)
+
+    if op == "add":
+        client.add_user(user, "")
+    elif op == "remove":
+        client.remove_user(user, "")
+
     # Close!
     transport.close()
 
-if __name__== "__main__":
+
+def main():
+    for line in stdin:
+        op, user_id, username, score = line.split(' ')
+        operate(op, int(user_id), username, int(score))
+
+
+if __name__ == "__main__":
     main()
+
