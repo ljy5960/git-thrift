8ce3aef1e25a1f4b044bbda09ebbec6bb6153690
ljy <2736289464@qq.com>
2024-09-19 17:08:01 +0800

    implemment save-client

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -2,10 +2,14 @@
 // You should copy it to another filename to avoid overwriting it.
 
 #include "match_server/Match.h"
+#include "save_client/Save.h"
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
+#include <thrift/transport/TTransportUtils.h>
+#include <thrift/transport/TSocket.h>
+
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -18,8 +22,8 @@ using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
 using namespace std;
-
-using namespace  ::match_service;
+using namespace ::save_service;
+using namespace ::match_service;
 
 
 struct Task
@@ -40,6 +44,24 @@ class Pool
     public:
         void save_result(int a,int b){
             printf("Match result:%d %d\n",a,b);
+
+
+
+            std::shared_ptr<TTransport> socket(new TSocket("123.57.67.128", 9090));
+            std::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+            std::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+            SaveClient client(protocol);
+
+            try {
+                transport->open();
+                int res = client.save_data("acs_13355", "7f475cf0", a, b);
+                if(!res) puts("success");
+                else puts("failed");
+                transport->close();
+            } catch (TException& tx) {
+                cout << "ERROR: " << tx.what() << endl;
+            }
+
         }
         void match(){
             while(users.size()>1)
